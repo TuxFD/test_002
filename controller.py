034695023e7b44b8be0ed598d1d1ce0d87b06711
TuxFD <tuxfd@yandex.ru>
2024-07-25 23:37:06 +0300

    finalizing

--- controller.py
+++ controller.py
@@ -1,31 +1,33 @@
-# import time
-import queue
+import time
 from multiprocessing import Process, Queue
-
-# additional data
-from source import tasks_list
 
 
 class ProcessController:
     def __init__(self) -> None:
         self.all_tasks = Queue()
         self.run_tasks = Queue()
-        self.timeout_tasks = Queue()
+        self.timeout_tasks = int(0)
         self.done_tasks = Queue()
         self.proc = []
+        self.proc_timer = {}
         self.limit = 0
 
-    def wait(self):
-        return self.run_tasks.qsize()
+    # def wait(self):
+    #     return self.run_tasks.qsize()
 
     def wait_count(self):
         return self.all_tasks.qsize()
-    
+
     def alive_count(self):
         return self.run_tasks.qsize()
 
-    def set_max_proc(self, n: int):
-        self.limit = n
+    def set_max_proc(self, n):
+        num = int(n)
+        if num > 0:
+            self.limit = n
+        else:
+            print("Invalid processes number")
+            exit(1)
 
     def start(self, tasks: list, max_exec_time: float):
         try:
@@ -44,7 +46,9 @@ class ProcessController:
             while self.wait_count() > 0:
 
                 # ==== [3] ==== Create limited run_tasks Queue
-                while not self.all_tasks.empty() and self.run_tasks.qsize() < self.limit:
+                while (
+                    not self.all_tasks.empty() and self.run_tasks.qsize() < self.limit
+                ):
                     my_task = self.all_tasks.get()
                     self.run_tasks.put(my_task)
                 else:
@@ -61,38 +65,51 @@ class ProcessController:
                 while self.alive_count() > 0:
                     try:
                         runtask = self.run_tasks.get()
-                        print('TASK IN PROCESS:', runtask, 'ALIVE COUNT: ', self.alive_count())
-                        # try:
-                        #     p = Process(target=runtask[0], args=runtask[1])
-                        #     self.proc.append(p)
-                        #     p.start()
-                        # except Exception as e:
-                        #     print("==== ERROR ====: ", e)
+                        print(
+                            "TASK IN PROCESS:",
+                            runtask,
+                            "ALIVE COUNT: ",
+                            self.alive_count(),
+                        )
+                        try:
+                            runtask[1].append(self.done_tasks)
+                            p = Process(
+                                target=runtask[0],
+                                args=runtask[1],
+                            )
+                            p.start()
+                            p_start = time.time()
+                            self.proc.append(p)
+                            self.proc_timer[p.name] = p_start
+                            # ==== [4] ==== Wait for limited queue tasks to finish
+                            for p in self.proc:
+                                p.join(max_exec_time)  # TIMEOUT
+
+                                # ==== [5] ==== Processes terminator
+                                while p.is_alive():
+                                    p_started = self.proc_timer[p.name]
+                                    if time.time() - p_started - 1 < max_exec_time:
+                                        p.terminate()
+                                        self.timeout_tasks += 1
+
+                        except Exception as e:
+                            print()
+                            print("==== ERROR ====: ", e)
 
                     except Exception as e:
                         print()
                         print("==== ERROR ====: ", e)
-                
-                else:
-                    print('DONE!')
 
-                    # ==== [4] ==== Wait for limited queue tasks to finish
-                    # for p in self.proc:
-                    #     p.join(max_exec_time)  # TIMEOUT
+                else:
+                    print("DONE!")
+                    print("")
 
             else:
-                print("QUEUE EMPTY!")
+                print("QUEUE EMPTY! RESULTs:")
+                while not self.done_tasks.empty():
+                    print(self.done_tasks.get())
+                print("PROCESSES TIMED OUT:", self.timeout_tasks)
 
         except Exception as e:
+            print()
             print("==== ERROR ====: ", e)
-
-
-# ======== TODO: delete this later
-def main():
-    pc = ProcessController()
-    pc.set_max_proc(3)
-    pc.start(tasks_list, max_exec_time=1)
-
-
-if __name__ == "__main__":
-    main()
--- main.py
+++ main.py
@@ -1,12 +1,46 @@
-from source import tasks_list
+import time
 from controller import ProcessController
+
+
+def custom_1(a, b, queueu):
+    time.sleep(0.5)
+    queueu.put("custom_1 returned ")
+
+
+def custom_2(a, b, queueu):
+    time.sleep(1.5)
+    queueu.put("custom_2 returned ")
+
+
+def custom_3(a, b, queueu):
+    time.sleep(3)
+    queueu.put("custom_3 returned ")
+
+
+def custom_4(a, b, queueu):
+    time.sleep(5)
+    queueu.put("custom_4 returned ")
+
+
+def custom_5(a, b, queueu):
+    time.sleep(100)
+    queueu.put("custom_5 returned ")
+
+
+tasks_list = [
+    (custom_1, [2, 2]),
+    (custom_2, [2, 2]),
+    (custom_3, [2, 2]),
+    (custom_4, [2, 2]),
+    (custom_5, [2, 2]),
+]
 
 
 # ========
 def main():
     pc = ProcessController()
-    pc.set_max_proc(3)
-    pc.start(tasks_list, max_exec_time=1)
+    pc.set_max_proc(2)
+    pc.start(tasks_list, max_exec_time=4)
 
 
 if __name__ == "__main__":
